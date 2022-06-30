68ad9f1a1b91ee05e64fb85212e5f5d3cd2c8072
Aymeric Guth <aymeric.guth@protonmail.com>
2022-07-01 00:11:36 +0200

    essai setuptools

--- fxenc/index/__init__.py
+++
@@ -1,9 +0,0 @@
-from . import japanese
-from . import latin
-
-
-STANDARD_MAP_BYTES: dict[bytes, bytes] = japanese.STANDARD_MAP_BYTES | latin.STANDARD_MAP_BYTES
-STANDARD_MAP_STR: dict[str, str] = japanese.STANDARD_MAP_STR | latin.STANDARD_MAP_STR
-COMBINING_SUPPORTED: set[str] = japanese.COMBINING_SUPPORTED | latin.COMBINING_SUPPORTED
-COMBINING_FULL: set[str] = japanese.COMBINING_FULL | latin.COMBINING_FULL
-SUPPORTED_RANGE: set[str] = japanese.SUPPORTED_RANGE | latin.SUPPORTED_RANGE
